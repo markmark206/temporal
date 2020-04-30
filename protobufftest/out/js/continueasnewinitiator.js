2577222294c95faee44d27c54daaa43bde00b868
Mark Markaryan <markmark@temporal.io>
2020-04-30 00:50:39 -0700

    adding generated files

---
+++ protobufftest/out/js/continueasnewinitiator.js
@@ -0,0 +1,22 @@
+// source: option3.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.option3.ContinueAsNewInitiator');
+
+/**
+ * @enum {number}
+ */
+proto.option3.ContinueAsNewInitiator = {
+  CONTINUE_AS_NEW_INITIATOR_UNSPECIFIED: 0,
+  CONTINUE_AS_NEW_INITIATOR_DECIDER: 1,
+  CONTINUE_AS_NEW_INITIATOR_RETRY: 2,
+  CONTINUE_AS_NEW_INITIATOR_CRON_SCHEDULE: 3
+};
+
